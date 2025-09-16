c5d3827a0d761700c812df54d1f8fd04b4f57ecf
aldair <torrezaldair777@gmail.com>
2025-09-16 18:09:47 -0400

    useless stuff deleted

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,17 +1,17 @@
 return {
 	{
 		"williamboman/mason.nvim",
-    lazy = false,
+		lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = { "lua_ls", "gopls", "htmx", "html", "ts_ls", "tailwindcss", "pyright" },
@@ -20,35 +20,18 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
-    lazy = false,
+		lazy = false,
 		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
-			lspconfig.htmx.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-			lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-			lspconfig.tailwindcss.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-      })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
+      vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", '<leader>rn', vim.lsp.buf.rename, { noremap = true, silent = true, desc = 'Rename variable' })
+			vim.keymap.set(
+				"n",
+				"<leader>rn",
+				vim.lsp.buf.rename,
+				{ noremap = true, silent = true, desc = "Rename variable" }
+			)
 		end,
 	},
 }
--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -2,7 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- MINE
 vim.keymap.set('n', '<leader>w', ':set wrap!<CR>', { noremap = true, silent = true })
 
